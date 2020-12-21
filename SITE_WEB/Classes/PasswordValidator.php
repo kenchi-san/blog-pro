4ee5342009a506b36e3a1bfec85866718be8e612
hugo charon <charon.hugo@gmail.com>
2020-12-21 12:50:23 +0100

    refactoring / quality

--- SITE_WEB/Classes/PasswordValidator.php
+++ SITE_WEB/Classes/PasswordValidator.php
@@ -15,17 +15,17 @@ class PasswordValidator extends AbstractController
         $view = new View('/frontViews/updatePasswordPage');
 
         if ($user == false) {
-             $view->renderView(['invalidToken' => true]);
+            return  $view->renderView(['invalidToken' => true]);
         }
         if ( $this->request->post('password_1') != $this->request->post('password_2')) {
 
-            $view->renderView(['invalidePassword' => true]);
+           return $view->renderView(['invalidePassword' => true]);
 
         }
         if (empty($this->request->post('password_1'))) {
-            $view->renderView(['invalidePassword2' => true]);
+            return $view->renderView(['invalidePassword2' => true]);
         }
-        return true;
+
 
 
     }
